644cec32120d2198397f52839c09a8ab68d1f2da
Galen Stevens <galen.stevens@gmail.com>
2016-04-20 01:01:15 -0400

    Add Param annotation for status.

--- src/main/java/org/grjug/bootlab/domain/LaunchRepository.java
+++ src/main/java/org/grjug/bootlab/domain/LaunchRepository.java
@@ -2,10 +2,11 @@ package org.grjug.bootlab.domain;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface LaunchRepository extends CrudRepository<Launch, Long> {
 
-    List<Launch> findByStatus(int status);
+    List<Launch> findByStatus(@Param("status") int status);
     
     default List<Launch> findByStatus(LaunchStatus status) {
         return LaunchRepository.this.findByStatus(status.intValue());
